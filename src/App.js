6e4976f8b8d2af6d5772f86a96ad08b5a51a553a
ursula-paul <ursula.paul@decagon.dev>
2022-03-27 17:49:14 +0100

    initialised the project and setup my project architecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
+import {Component} from 'react'
+
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+
+  constructor(){
+      super();
+
+      this.state = {
+        monsters:[],
+        searchField:'',
+        
+      };
+      // console.log('constructor')
+    }
+
+    componentDidMount(){
+      // console.log('componetDidMount')
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response)=> response.json() )
+      .then((users)=> this.setState(
+
+        () =>{
+        return{monsters:users}
+      },
+
+      ()=>{
+        console.log(this.setState)
+      }
+      
+      ))
+    }
+    // this only gets called and initialize once when out class components  initialises
+
+    onSearchChange =(event)=>{
+          console.log(event.target.value)
+          const searchField =event.target.value.toLocaleLowerCase();
+          this.setState(()=>{
+            return {searchField};
+          })
+        }
+    render(){
+      // console.log('render')
+      // distructuring them into variables so we don't use this.state every thime we need them
+      const {monsters,searchField} =this.state
+      const {onSearchChange} = this
+
+      const filteredMonsters =monsters.filter((monster)=>{
+            return monster.name.toLocaleLowerCase().includes(searchField);
+          });
+
+
+      return (
+      <div className="App">
+        <input className='search-box' 
+         type='search' 
+        placeholder='Search monsters' 
+        onChange={onSearchChange}
+
+        />
+        {filteredMonsters.map((monster)=>{
+          return (
+            <div key = {monster.id}>
+            <h1>{monster.name}</h1>  
+            </div>
+          )
+        })}  
+      </div>
+    )
+  };
+
+
 }
 
 export default App;
